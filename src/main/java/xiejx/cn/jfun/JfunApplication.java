aaa6752ce54fe5af45319406198c12af6f501641
谢境勋 <787824374@qq.com>
2019-03-11 17:12:44 +0800

    Initial commit

---
+++ src/main/java/xiejx/cn/jfun/JfunApplication.java
@@ -0,0 +1,13 @@
+package xiejx.cn.jfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfunApplication.class, args);
+    }
+
+}
